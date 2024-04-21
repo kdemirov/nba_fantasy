d3fde0fbe123af019375c4f2de4694ce67ca12ef
kdemirov <kazim.demirov@gmail.com>
2024-04-21 13:18:21 +0200

    Remove unneeded files

--- src/main/java/mk/ukim/finki/nbafantasy/model/Observer.java
+++
@@ -1,5 +0,0 @@
-package mk.ukim.finki.nbafantasy.model;
-
-public interface Observer {
-    void update(Notifications notifications);
-}
--- src/main/java/mk/ukim/finki/nbafantasy/model/exceptions/InvalidUsernameOrPasswordException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.nbafantasy.model.exceptions;
-
-public class InvalidUsernameOrPasswordException extends RuntimeException{
-    public InvalidUsernameOrPasswordException(){
-        super("Username and password can not be empty");
-    }
-}
