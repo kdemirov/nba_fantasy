418f6e91d0c96a85f9b302e915ff8f41e7fb8b27
kdemirov <kazim.demirov@gmail.com>
2024-04-21 13:19:27 +0200

    Fix MyTeamFilter and ValidationFilter

--- src/main/java/mk/ukim/finki/nbafantasy/web/filters/MyTeamFilter.java
+++ src/main/java/mk/ukim/finki/nbafantasy/web/filters/MyTeamFilter.java
@@ -1,8 +1,9 @@
 package mk.ukim.finki.nbafantasy.web.filters;
 
+import mk.ukim.finki.nbafantasy.config.Constants;
 import mk.ukim.finki.nbafantasy.model.User;
 import mk.ukim.finki.nbafantasy.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import javax.servlet.*;
@@ -11,7 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+/**
+ * Checks if user has selected his own team if not the user
+ * is redirected to transfers page.
+ */
 @WebFilter
 public class MyTeamFilter implements Filter {
 
@@ -22,20 +26,26 @@ public class MyTeamFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request=(HttpServletRequest) servletRequest;
-        HttpServletResponse response=(HttpServletResponse) servletResponse;
-        User user=null;
-        try {
-            user = this.userService.findByUsername(request.getRemoteUser());
-        }catch (UsernameNotFoundException e){
-            System.out.println(e.getMessage());
-        }
-        String path=request.getServletPath();
-        if(user!=null&&user.getMyTeam().size()<5&&path.equals("/myteam")){
-            response.sendRedirect("/transfers");
-        }else{
-            filterChain.doFilter(servletRequest,servletResponse);
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        User user = null;
+        String path = request.getServletPath();
+        if (path.equals(Constants.MY_TEAM_URL)) {
+            try {
+                String username = SecurityContextHolder.getContext().getAuthentication().getName();
+                user = username != null ? this.userService.findByUsername(username) : null;
+            } catch (UsernameNotFoundException o_O) {
+                System.out.println(o_O.getMessage());
+            }
+            if (user != null && user.getMyTeam().size() < Constants.ALLOWED_TEAM_SIZE) {
+                response.sendRedirect(Constants.TRANSFERS_URL);
+            } else {
+                filterChain.doFilter(servletRequest, servletResponse);
+            }
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
         }
     }
 }
